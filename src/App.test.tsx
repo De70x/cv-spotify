da2a7402b3d15a2c7d7fe339862125c54126de7a
De70x <antoine.deloffre7@gmail.com>
2022-09-04 07:15:17 +0200

    render Menu

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App, { Menu } from "./App";
 
-test('renders learn react link', () => {
+test("render title CV Spotify", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/CV Spotify/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("render the menu", () => {
+  render(<Menu />);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+export function Menu() {
+  return null;
+}
+
 function App() {
   return (
     <div>
       <h1>CV Spotify</h1>
+      <Menu />
     </div>
   );
 }
