c72d2772fcd0b99550c368bf1baa670abada8139
De70x <antoine.deloffre7@gmail.com>
2022-09-05 08:30:56 +0200

    New test before creating card container

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,25 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Menu from "./Menu/Menu";
+import CardContainer from "./Main/CardContainer";
+import App from "./App";
+import { TestId } from "./Const/TestId";
 
+// some tests just to be sure each component exists
 test("render the menu", () => {
   render(<Menu />);
+});
+
+test("render the card container", () => {
+  render(<CardContainer />);
+});
+
+test("default app should display at least the menu and the cardContainer", () => {
+  render(<App />);
+
+  const menu = screen.getByTestId(TestId.MENU);
+  const cardContainer = screen.getByTestId(TestId.CARD_CONTAINER);
+
+  expect(menu).toBeInTheDocument();
+  expect(cardContainer).toBeInTheDocument();
 });
