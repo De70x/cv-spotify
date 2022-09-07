4404479c60e71077b1aab4bc0dac80c0ad6e4fe5
De70x <antoine.deloffre7@gmail.com>
2022-09-07 11:46:02 +0200

    describe syntaxt for lisibility

--- src/Menu/MenuItem/MenuItem.test.tsx
+++ src/Menu/MenuItem/MenuItem.test.tsx
@@ -9,25 +9,28 @@ let name: HTMLElement;
 
 process.env.PUBLIC_URL = "http://localhost";
 
-test("menuItem should be in the doc", () => {
-  render(<MenuItem />);
-  menuItem = screen.getByTestId(TestId.MENU_ITEM, { exact: false });
-  expect(menuItem).toBeInTheDocument();
-});
-
-test("MenuItems should have default values if not set", () => {
-  render(<MenuItem />);
-  logo = screen.getByTestId("logo");
-  name = screen.getByTestId(/name/);
-  expect(logo.alt).toEqual("Unknown.png");
-  expect(name.innerHTML).toEqual("Unknown");
-});
-
-test("MenuItems should have props correctly set", () => {
-  render(<MenuItem name={"test"} logo={"logo.png"} />);
-  logo = screen.getByTestId("logo");
-  name = screen.getByTestId("nametest");
-  expect(logo.alt).toEqual("logo.png");
-  expect(logo.src).toEqual(process.env.PUBLIC_URL + "/logo.png");
-  expect(name.innerHTML).toEqual("test");
+describe("Menu Item tests", () => {
+  it("Should be in the doc", () => {
+    render(<MenuItem />);
+    menuItem = screen.getByTestId(TestId.MENU_ITEM, { exact: false });
+    expect(menuItem).toBeInTheDocument();
+  });
+  it("should have default values if not set", () => {
+    render(<MenuItem />);
+    logo = screen.getByTestId("logo");
+    name = screen.getByTestId(/name/);
+    expect(logo.alt).toEqual("Unknown.png");
+    expect(name.innerHTML).toEqual("Unknown");
+  });
+  it("should have props correctly set", () => {
+    render(<MenuItem name={"test"} logo={"logo.png"} />);
+    logo = screen.getByTestId("logo");
+    name = screen.getByTestId("nametest");
+    expect(logo.alt).toEqual("logo.png");
+    expect(logo.src).toEqual(process.env.PUBLIC_URL + "/logo.png");
+    expect(name.innerHTML).toEqual("test");
+  });
+  it("should be selectable", () => {
+    render(<MenuItem selected={true} />);
+  });
 });
