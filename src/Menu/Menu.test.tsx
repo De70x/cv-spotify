d8f2ac626e90bb97898445cec03f0c1e23763dff
De70x <antoine.deloffre7@gmail.com>
2022-09-07 10:44:59 +0200

    using describe to be more readable

--- src/Menu/Menu.test.tsx
+++ src/Menu/Menu.test.tsx
@@ -2,29 +2,33 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Menu from "./Menu";
 import { TestId } from "../Const/TestId";
+import { MenuEntry } from "../Const/MenuEntry";
 
-let menu: HTMLElement;
+describe("Meu tests", () => {
+  it("should be in the doc", () => {
+    render(<Menu />);
+    const menu = screen.getByTestId(TestId.MENU);
+    expect(menu).toBeInTheDocument();
+  });
+  it("should be a nav item", () => {
+    render(<Menu />);
+    const menu = screen.getByTestId(TestId.MENU);
+    expect(menu.outerHTML).toContain("</nav>");
+  });
+  it("should have some entries", () => {
+    render(<Menu />);
+    const menu = screen.getByTestId(TestId.MENU);
+    if (menu === null || menu.firstElementChild === null) {
+      throw new Error("No menu entries");
+    }
+  });
 
-function initMenu() {
-  render(<Menu />);
-  menu = screen.getByTestId(TestId.MENU);
-}
-
-beforeAll(() => {
-  initMenu();
-});
-
-test("menu should be in the doc", () => {
-  expect(menu).toBeInTheDocument();
-});
-
-test("menu should be a nav item", () => {
-  expect(menu.outerHTML).toContain('<nav id="menu"');
-  expect(menu.outerHTML).toContain("</nav>");
-});
-
-test("menu should have some entries", () => {
-  if (menu === null || menu.firstElementChild === null) {
-    throw new Error("No menu entries");
-  }
+  it("should know the current page", () => {
+    render(<Menu page={MenuEntry.CV} />);
+  });
+  it("Home should be selected by default", () => {
+    render(<Menu />);
+    const menuItem = screen.getByTestId(TestId.MENU_ITEM + "Home");
+    console.log(menuItem.getAttributeNames());
+  });
 });
