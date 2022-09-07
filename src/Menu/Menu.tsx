10889766c89434c00c369970dcc95fd3f34b505c
De70x <antoine.deloffre7@gmail.com>
2022-09-07 10:45:42 +0200

    preparing navigation menu

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Menu.css";
 import MenuItem from "./MenuItem/MenuItem";
+import { MenuEntry } from "../Const/MenuEntry";
+import { TestId } from "../Const/TestId";
 
-const Menu = () => {
+interface MenuProps {
+  onChange?: any;
+  page?: MenuEntry;
+}
+
+const Menu = (props: MenuProps) => {
   return (
-    <nav id="menu" data-testid="menu">
+    <nav id="menu" data-testid={TestId.MENU}>
       <div>
         <img
           src={process.env.PUBLIC_URL + "/Spotify_logo.svg.png"}
@@ -12,8 +19,8 @@ const Menu = () => {
           alt="Spotify logo"
         />
       </div>
-      <MenuItem name="Home" logo="home.png" />
-      <MenuItem name="CV" logo="cv.png" />
+      <MenuItem name="Home" logo="home.png" data-testid={TestId.HOME_ENTRY} />
+      <MenuItem name="CV" logo="cv.png" data-testid={TestId.CV_ENTRY} />
     </nav>
   );
 };
