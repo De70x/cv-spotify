ff67ae963a585539bd0734a5295994e3fe893d71
De70x <antoine.deloffre7@gmail.com>
2022-09-07 08:28:35 +0200

    tests are more readable like this

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,27 +1,29 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import App from "./App";
 import Menu from "./Menu/Menu";
 import CardContainer from "./Main/CardContainer";
-import App from "./App";
 import { TestId } from "./Const/TestId";
 
 // some tests just to be sure each component exists
-test("render the menu", () => {
-  render(<Menu />);
+describe("Testing component", () => {
+  it("Should be able to render Menu component", () => {
+    render(<Menu />);
+  });
+  it("Should be able to render CardContainer component", () => {
+    render(<CardContainer />);
+  });
 });
 
-test("render the card container", () => {
-  render(<CardContainer />);
+describe("Default home page", () => {
+  it("Should contain menu and card container", () => {
+    render(<App />);
+
+    const menu = screen.getByTestId(TestId.MENU);
+    const cardContainer = screen.getByTestId(TestId.CARD_CONTAINER);
+
+    expect(menu).toBeInTheDocument();
+    expect(cardContainer).toBeInTheDocument();
+  });
+  it("Should be home page by default", () => {});
 });
-
-test("default app should display at least the menu and the cardContainer", () => {
-  render(<App />);
-
-  const menu = screen.getByTestId(TestId.MENU);
-  const cardContainer = screen.getByTestId(TestId.CARD_CONTAINER);
-
-  expect(menu).toBeInTheDocument();
-  expect(cardContainer).toBeInTheDocument();
-});
-
-test("App should display Home first");
