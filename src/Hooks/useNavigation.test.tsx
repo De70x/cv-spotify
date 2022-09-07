705cab58f3e3af155cc815a09f1e19e0dbef3d59
De70x <antoine.deloffre7@gmail.com>
2022-09-07 09:35:52 +0200

    remove unused hook

--- src/Hooks/useNavigation.test.tsx
+++
@@ -1,9 +0,0 @@
-import { renderHook } from "@testing-library/react";
-import useNavigation from "./useNavigation";
-
-describe("useNavigation hook", () => {
-  it("should exist", () => {
-    const { result } = renderHook(() => useNavigation());
-    expect(result.current).toBeDefined();
-  });
-});
